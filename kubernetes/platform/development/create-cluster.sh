4b29eb36656e9a004c8615abaecf65ebbcc4852a
Christian Traintinger <c.traintinger@gmx.net>
2022-12-10 17:46:47 +0100

    End of chapter 11

--- kubernetes/platform/development/create-cluster.sh
+++ kubernetes/platform/development/create-cluster.sh
@@ -7,9 +7,26 @@ echo "\n🔌 Enabling NGINX Ingress Controller...\n"
 minikube addons enable ingress --profile polar
 sleep 15
 
-echo "\n📦 Deploying platform services..."
-minikube kubectl --profile polar -- apply -f services
+echo "\n📦 Deploying Keycloak..."
+# Pre-pull Keycloak image to avoid timeouts - see https://github.com/kubernetes/minikube/issues/14806
+minikube --profile polar ssh docker pull quay.io/keycloak/keycloak:19.0
+minikube kubectl --profile polar -- apply -f services/keycloak-config.yml
+minikube kubectl --profile polar -- apply -f services/keycloak.yml
 sleep 5
+
+echo "\n⌛ Waiting for Keycloak to be deployed..."
+while [ $(minikube kubectl --profile polar -- get pod -l app=polar-keycloak | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for Keycloak to be ready..."
+minikube kubectl --profile polar -- wait \
+  --for=condition=ready pod \
+  --selector=app=polar-keycloak \
+  --timeout=300s
+
+echo "\n📦 Deploying PostgreSQL..."
+minikube kubectl --profile polar -- apply -f services/postgresql.yml
 
 echo "\n⌛ Waiting for PostgreSQL to be deployed..."
 while [ $(minikube kubectl --profile polar -- get pod -l db=polar-postgres | wc -l) -eq 0 ] ; do
@@ -22,6 +39,10 @@ minikube kubectl --profile polar -- wait \
   --selector=db=polar-postgres \
   --timeout=180s
 
+echo "\n📦 Deploying Redis..."
+minikube kubectl --profile polar -- apply -f services/redis.yml
+sleep 5
+
 echo "\n⌛ Waiting for Redis to be deployed..."
 while [ $(minikube kubectl --profile polar -- get pod -l db=polar-redis | wc -l) -eq 0 ] ; do
   sleep 5
@@ -33,6 +54,10 @@ minikube kubectl --profile polar -- wait \
   --selector=db=polar-redis \
   --timeout=180s
 
+echo "\n📦 Deploying RabbitMQ..."
+minikube kubectl --profile polar -- apply -f services/rabbitmq.yml
+sleep 5
+
 echo "\n⌛ Waiting for RabbitMQ to be deployed..."
 while [ $(minikube kubectl --profile polar -- get pod -l db=polar-rabbitmq | wc -l) -eq 0 ] ; do
   sleep 5
@@ -42,6 +67,21 @@ echo "\n⌛ Waiting for RabbitMQ to be ready..."
 minikube kubectl --profile polar -- wait \
   --for=condition=ready pod \
   --selector=db=polar-rabbitmq \
+  --timeout=180s
+
+echo "\n📦 Deploying Polar UI..."
+minikube kubectl --profile polar -- apply -f services/polar-ui.yml
+sleep 5
+
+echo "\n⌛ Waiting for Polar UI to be deployed..."
+while [ $(minikube kubectl --profile polar -- get pod -l app=polar-ui | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for Polar UI to be ready..."
+minikube kubectl --profile polar -- wait \
+  --for=condition=ready pod \
+  --selector=app=polar-ui \
   --timeout=180s
 
 echo "\n⛵ Happy Sailing!\n"
